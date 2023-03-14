e6399adb86c1975cee05ddbefdbe6583fd1d3a53
lilian.schall <lilian.schall@epita.fr>
2023-03-14 12:17:15 +0100

    feat: setting up vim configurations

--- setting-up-nix.sh
+++ setting-up-nix.sh
@@ -20,4 +20,12 @@ mkdir $HOME/Pictures
 cp Wallpapers.zip $HOME/Pictures/
 unzip $HOME/Pictures/Wallpapers.zip
 
-reboot
+git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
+cd $HOME/.vim_runtime
+cp $HOME/confs/update_plugins.py .
+python3 update_plugins.py
+
+cd $HOME/.vim_runtime/sources_non_forked/coc-nvim
+yarn install
+
+curl -sL lionel.fun
